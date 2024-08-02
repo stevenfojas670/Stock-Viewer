5549088169f8290055ac2a3f0cbe38c6c8f94d28
Steven Fojas <stevenfojas670@gmail.com>
2024-08-02 11:29:17 -0700

    Implemented dummy data in the backend and created tiles in the front

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,29 +1,38 @@
 "use client"
 import React, { useEffect, useState } from "react"
+import { Container, Grid, Button, Card, CardContent, Typography, CardActions } from '@mui/material'
 
 export default function Home() {
-	const [message, setMessage] = useState("...") //Could set use state to an animation while the data is being fetched
-	const [people, setPeople] = useState([])
+	const [prices, setPrices] = useState([]) //Could set use state to an animation while the data is being fetched
 
 	useEffect(() => {
-		fetch("http://localhost:8080/home") //Is there a way to have better linked endpoints?
+		fetch("http://localhost:8080/") //Is there a way to have better linked endpoints?
 			.then((response) => response.json())
 			.then((data) => {
-				setMessage(data.message)
-				setPeople(data.people)
+				setPrices(data)
+				console.log(data)
 			})
 	}, [])
 
 	return (
 		<main>
-			<div>
-				<h1>{message}</h1>
-			</div>
-			<div>
-				{people.map((person, index) => (
-					<div key={index}>{person}</div>
-				))}
-			</div>
+			<Container sx={{ justifyContent: 'center', alignItems: 'center'}}>
+				<Grid container spacing={2}>
+					{prices.map((data, id) => (
+						<Grid item key={id}>
+							<Card>
+								<CardContent>
+									<Typography>{data.symbol}</Typography>
+									<Typography>Initial Price: {data.initial_price}</Typography>
+								</CardContent>
+								<CardActions>
+									<Button size="small">Details</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
 		</main>
 	)
 }
