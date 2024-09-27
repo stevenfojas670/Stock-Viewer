7b4d3bfec71e7e3627239896cbc4ce3e79bf0747
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2024-09-26 22:03:37 -0700

    Removed mongoDb

--- server/server.py
+++ server/server.py
@@ -1,10 +1,8 @@
 import os
-import pymongo
 from flask import Flask, redirect, url_for, request, jsonify, url_for, g
 from flask_caching import Cache
 from markupsafe import escape
 from flask_cors import CORS
-from pymongo import MongoClient
 
 # Blueprints imports
 from blueprints.general.home import home_bp
@@ -17,13 +15,6 @@ CORS(app)
 app.config['AV_KEY'] = os.environ.get('AV_KEY')
 app.config['SECRET_KEY'] = os.environ.get('JWT_KEY')
 
-client = MongoClient('localhost', 27017)
-db = client['StockTrackerDB']
-users_collection = db['Users']
-
-@app.before_request
-def before_request():
-    g.db = db
 
 # Going to need a teardown request to clear the global variable
 
